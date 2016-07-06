9ffbd1b7bf8599ef196dd9aabc016a41f18fb2df
Daniel Grace <negativeview@gmail.com>
2016-07-06 00:46:16 +0000

    Another syntax error.

--- d20.js
+++ d20.js
@@ -85,8 +85,7 @@ class D20Character extends BaseCharacter {
 						},
 						set: (value) => {
 							data[score] = value;
-						}
-						value: tmp,
+						},
 						writable: true
 					}
 				);
